b4bbb4d5f68306f1d0daa77614347846bec093ec
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-07 15:35:18 -0500

    Navigate

--- AuthRequired.jsx
+++ AuthRequired.jsx
@@ -1,11 +1,14 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom"
 
 export default function AuthRequired() {
     // Check the authenticated status of the user
-    const authenticated = true
+    const authenticated = false
     // If they're NOT authenticated
     // send the user to the login page
+    if (authenticated === false) {
+        return <Navigate to="login" />
+    }
 
     // Mini challenge: write this logic 👇
     // If they ARE authenticated
