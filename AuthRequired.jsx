5277d5a70eb2792d5f1bbcd547ac962a60fc6b3d
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-07 15:24:54 -0500

    Implementing "auth" (FDCP)

--- AuthRequired.jsx
+++ AuthRequired.jsx
@@ -3,9 +3,14 @@ import { Outlet } from "react-router-dom"
 
 export default function AuthRequired() {
     // Check the authenticated status of the user
+    const authenticated = true
     // If they're NOT authenticated
     // send the user to the login page
+
+    // Mini challenge: write this logic 👇
     // If they ARE authenticated
     // Render the Outlet
-    return <Outlet />
+    if (authenticated === true) {
+        return <Outlet />
+    }
 }
